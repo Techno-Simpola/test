ecf0c46b74dbf5d00d48d08e139b7abbd35b9bae
Simpola <74448357+Techno-Simpola@users.noreply.github.com>
2023-01-19 02:06:23 +0530

    updating admin.py

--- response/admin.py
+++ response/admin.py
@@ -1,1 +1,15 @@
+from django.contrib import admin
+from .models import Question,Response
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from accounts.models import Profile
 
+# Register your models here.
+
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ('profile','question','response')
+    search_fields = ['profile__user__username','question__ques_round','response',]
+    list_filter = ('profile','question')
+
+admin.site.register(Question)
+admin.site.register(Response, ResponseAdmin)
